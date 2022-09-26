ac7eeeeac141a57ef7a7e4101737f04f6c18e94b
mle149 <leemi546@gmail.com>
2022-09-27 10:34:01 +1300

    Initial commit

---
+++ Java to Python.py
@@ -0,0 +1,31 @@
+PI = 3.14159;
+
+#Function for Area of Square
+def area_square(width):
+    return (width * width);
+
+#Function for Area of Circle
+def area_circle(radius):
+    return (PI * (radius*radius) );
+
+#Function to display Palindromes
+
+#Display menu 
+print("Calculations");
+print("1. Calculate area of a square");
+print("2. Calculate area of a circle");
+print("3. Display palindromes up to 1,000");
+
+#Get user input for menu option
+user_input = input("Enter an option: ")
+
+#Perform function depending on user input
+#if user_input == 1:
+
+#elif user_input == 2:
+
+#elif user_input == 3:
+
+#else:
+    #print("Invalid input, please try again");
+    #display_menu;
