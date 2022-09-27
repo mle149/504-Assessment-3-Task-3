5c5653255fc45a9e2fa97da963f7c9f557698f7a
mle149 <leemi546@gmail.com>
2022-09-27 19:36:59 +1300

    Completed functions and main

--- Java to Python.py
+++ Java to Python.py
@@ -1,31 +1,70 @@
-PI = 3.14159;
+#Declare static variables
+PI = 3.14159
+
+# **** FUNCTIONS ****
 
 #Function for Area of Square
 def area_square(width):
-    return (width * width);
+    print (width * width)
 
 #Function for Area of Circle
 def area_circle(radius):
-    return (PI * (radius*radius) );
+    print (PI * (radius * radius) )
 
-#Function to display Palindromes
+#Function: Convert int to string
+def convert_to_string(x):
+    return str(x)
 
-#Display menu 
-print("Calculations");
-print("1. Calculate area of a square");
-print("2. Calculate area of a circle");
-print("3. Display palindromes up to 1,000");
+#Function: Reverse string 
+def reverse_string(x):
+  return x[::-1]
 
-#Get user input for menu option
-user_input = input("Enter an option: ")
+#Function: Display Palindromes
+def palindrome():
+    #Loop through numbers 1-999
+    for i in range(1000):
+        x = convert_to_string(i)
+        y = reverse_string(x)
+        #check if the number is a palindrome
+        if x == y:
+            print(x)
 
-#Perform function depending on user input
-#if user_input == 1:
+#Function: Display menu
+def display_menu():
+    print("Calculations")
+    print("1. Calculate area of a square")
+    print("2. Calculate area of a circle")
+    print("3. Display palindromes up to 1,000")
 
-#elif user_input == 2:
+#Function: Get user input for menu option
+def user_input():
+    x = int(input("Enter an option: "))
+    return x
 
-#elif user_input == 3:
+#Function: Make sure userinput option is between 1-3
+#If selection is between 1-3, perform the appropriate function
+def menu_options(user_selection):
+    if user_selection >= 1 and user_selection <= 3:
+        if user_selection == 1:
+            #Call area square function
+            width = int(input("Please enter the width of a square: "))
+            area_square(width)
+        elif user_selection == 2:
+            #Call area circle function
+            radius = int(input("Please enter the radius of the circle: "))
+            area_circle(radius)
+        else:
+            #Call palindrome function
+            palindrome()
+    #User inputs number outside of 1-3 range
+    else:
+        print("\nInvalid option. Please enter a valid number between 1-3.\n")
 
-#else:
-    #print("Invalid input, please try again");
-    #display_menu;
+
+#   **** MAIN ****
+display_menu()
+user_selection = user_input()
+menu_options(user_selection)
+
+
+
