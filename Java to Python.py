20c0a93cdf6f3343f457b389490722eb9b94ee23
mle149 <leemi546@gmail.com>
2022-10-07 20:55:21 +1300

    Fixed Exception handling

--- Java to Python.py
+++ Java to Python.py
@@ -1,7 +1,14 @@
 #Declare static variables
+import sys
+
 PI = 3.14159
 i = 0 #Used in the MAIN while loop
 # **** FUNCTIONS ****
+
+#Function: Invalid input from user
+def invalid_input():
+    print("\nInvalid option. Please try again.\n")
+    input("Press Enter to continue...\n")
 
 #Function for Area of Square
 def area_square(width):
@@ -45,28 +52,28 @@ def user_input():
 #Function: Make sure userinput option is between 1-4
 #If selection is between 1-4, perform the appropriate function
 def menu_options(user_selection):
-    if user_selection >= 1 and user_selection <= 4:
-        if user_selection == 1:
-            #Call area square function
-            width = int(input("Please enter the width of a square: "))
-            area_square(width)
-        elif user_selection == 2:
-            #Call area circle function
-            radius = int(input("Please enter the radius of the circle: "))
-            area_circle(radius)
-        elif user_selection ==3:
-            #Call palindrome function
-            palindrome()
-        else:
-            quit()
-    #User inputs number outside of 1-3 range, print a invalid option to screen
+    #if user_selection >= 1 and user_selection <= 4:
+    if user_selection == 1:
+        #Call area square function
+        width = int(input("Please enter the width of a square: "))
+        area_square(width)
+    elif user_selection == 2:
+        #Call area circle function
+        radius = int(input("Please enter the radius of the circle: "))
+        area_circle(radius)
+    elif user_selection == 3:
+        #Call palindrome function
+        palindrome()
     else:
-        print("\nInvalid option. Please enter a valid number between 1-3.\n")
-
+        print("Goodbye!!!")
+        sys.exit(0)
 
 #   **** MAIN ****
-while i<1: #While loop to prompt the user to choose an option until they exit
-    display_menu()
-    user_selection = user_input()
-    menu_options(user_selection)
-    input("\nPress Enter to continue...\n")
+while i < 1: #While loop to prompt the user to choose an option until they exit 
+    try:
+        display_menu()
+        user_selection = user_input()
+        menu_options(user_selection)
+        input("\nPress Enter to continue...\n")
+    except Exception:
+        invalid_input()
